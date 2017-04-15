9ba78054861fdccc6db9233c38e77f2f66d745f7
tositeru <uedariki1122@gmail.com>
2017-04-15 15:38:01 +0900

    無視リスト更新

--- dx12/graphics/dx12/utility/Shader/Includer/MakeShaderSystemRootDirHeader.bat
+++ dx12/graphics/dx12/utility/Shader/Includer/MakeShaderSystemRootDirHeader.bat
@@ -3,6 +3,6 @@ set MY_SYSTEM_ROOT_PATH=%_VAR:\=/%
 echo #pargma once > %2ShaderSystemRootPath.h
 echo std::string gShaderSystemRootPath = "%MY_SYSTEM_ROOT_PATH%"; > %2ShaderSystemRootPath.h
 echo std::wstring gShaderSystemRootPathW = L"%MY_SYSTEM_ROOT_PATH%"; >> %2ShaderSystemRootPath.h
-echo // graphics/dx12/utility/Shader/shaderにあるhlsliファイルを自前のシェーダにインクルードかつCPP側でも使用したいときは> SunriseShaderSystemHeaderDir.txt
-echo // 下のパスをプロジェクトのインクルードパスに追加してください>> SunriseShaderSystemHeaderDir.txt
-echo %MY_SYSTEM_ROOT_PATH%>> SunriseShaderSystemHeaderDir.txt
+echo // graphics/dx12/utility/Shader/shaderにあるhlsliファイルを自前のシェーダにインクルードかつCPP側でも使用したいときは> HinodeShaderSystemHeaderDir.txt
+echo // 下のパスをプロジェクトのインクルードパスに追加してください>> HinodeShaderSystemHeaderDir.txt
+echo %MY_SYSTEM_ROOT_PATH%>> HinodeShaderSystemHeaderDir.txt
